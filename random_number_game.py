99dfb09f6bc32c0c9c116416f37ce9201bd692fc
Gen Vikat <47703789+vikatgen@users.noreply.github.com>
2019-11-22 01:19:46 +0200

    Random Number Game update
    added view statements

--- random_number_game.py
+++ random_number_game.py
@@ -3,8 +3,13 @@ import random
 userInput = int(input("Arva ära, mis number tuleb järgmisena (1 - 20): "))
 randomNumber = random.randint(1, 20)
 
-if userInput != randomNumber:
+
+if userInput < 1:
+    print("Kahjuks sisestasite liiga väikse arvu (1-20), proovi uuesti!")
+elif userInput > 20:
+    print("Kahjuks sisestasite liiga suure arvu (1-20), proovi uuesti!")
+elif userInput != randomNumber:
     print("Õnn ei olnud sinu poolel, proovi uuesti!")
 else:
-    print("Pihtas, põhjas! ", userInput, " on õige vastus!")
+    print("Pihtas, põhjas! ", userInput, " on õige vastus! Sa mõtled peagut nagu arvuti.")
 
